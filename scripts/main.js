358ba911639557a1f908ee243f2ebe660ca6aea5
dmaslan <55900387+dmaslan@users.noreply.github.com>
2019-10-11 15:58:25 -0400

    images and responsive

--- scripts/main.js
+++ scripts/main.js
@@ -18,3 +18,44 @@ function openCity(evt, cityName) {
     document.getElementById(cityName).style.display = "block";
     evt.currentTarget.className += " active";
 }
+
+var draggableElems = document.querySelectorAll('.draggable');
+// array of Draggabillies
+var draggies = []
+    // init Draggabillies
+for (var i = 0; i < draggableElems.length; i++) {
+    var draggableElem = draggableElems[i];
+    var draggie = new Draggabilly(draggableElem, {
+        // options...
+    });
+    draggies.push(draggie);
+}
+
+
+function showhide(divvy) {
+    document.getElementById(divvy).style.display = "block";
+}
+
+
+function changepic() {
+    var imgElement = document.getElementsByClassName('draggable');
+    var docHeight = window.innerHeight;
+    var docWidth = window.innerWidth;
+    var maxHeight = Math.max(0, docHeight - 300);
+    var maxWidth = Math.max(0, docWidth - 300);
+
+    if (window.innerWidth > 600) {
+        for (var p = 0; p < imgElement.length; p++) {
+            var top_position = (Math.random() * maxHeight).toFixed();
+            var left_position = (Math.random() * maxWidth).toFixed();
+            imgElement[p].style.top = top_position + "px";
+            imgElement[p].style.left = left_position + "px";
+        }
+    }
+}
+
+changepic();
+
+function showInstructions() {
+    document.getElementById("instructions").innerHTML = "*As images appear on the screen, feel free to drag them.";
+}
